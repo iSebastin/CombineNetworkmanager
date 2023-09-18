eb388c244c58743f5123195305136b78afa2eadd
sebastin-uproar <119631300+sebastin-uproar@users.noreply.github.com>
2023-09-18 13:42:27 -0400

    Initial Commit

---
+++ CombineNetworkmanager/CombineNetworkmanagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineNetworkmanagerApp.swift
+//  CombineNetworkmanager
+//
+//  Created by Victor Sebastin on 2023-09-18.
+//
+
+import SwiftUI
+
+@main
+struct CombineNetworkmanagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CombineNetworkmanager/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CombineNetworkmanager
+//
+//  Created by Victor Sebastin on 2023-09-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
